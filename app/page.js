29084a65980f88aa2b1241ea65bfb5966b979a23
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2025-02-03 23:50:37 +0000

    initial comment

---
+++ app/page.js
@@ -0,0 +1,38 @@
+'use client'
+
+import Image from "next/image";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { addItem, addName } from "@/lib/features/dataSlice";
+
+export default function Home() {
+
+  const [text,setText] = useState({})
+  const data = useSelector(state => state.data)
+  const dispatch = useDispatch()
+
+  function onSubmit(e) {
+    console.log(data)
+    dispatch(addName(''))
+    e.preventDefault()
+  }
+
+  
+  
+  console.log('index', data)
+  return (
+    <div className="w-screen h-screen flex flex-col bg-gradient-to-bl from-indigo-200 to-indigo-300">
+      <Form>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input id="name" name="name" placeholder="Enter your name..." onChange={(e)=>{dispatch(addName({name:e.target.value}))}} />
+          <Label for="item">Item</Label>
+          <Input id="item" name="item" placeholder="Enter items name..." onChange={(e)=>{dispatch(addItem({item:e.target.value}))}} />
+        </FormGroup>
+        <Button size="lg" color="success" onClick={(e)=>{onSubmit(e)}}>Submit</Button>
+      </Form>
+    </div>
+  );
+}
